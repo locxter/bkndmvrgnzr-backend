8f9e71fbe35d824d09a2cf2e34a366d9b9dab276
locxter <54595101+locxter@users.noreply.github.com>
2023-04-27 20:53:50 +0200

    Deleted unused DTO.

--- src/main/kotlin/com/github/locxter/bkndmvrgnzr/backend/user/api/UserResponseBriefDto.kt
+++
@@ -1,8 +0,0 @@
-package com.github.locxter.bkndmvrgnzr.backend.user.api
-
-data class UserResponseBriefDto(
-    val id: String = "",
-    val username: String = "",
-    val firstName: String = "",
-    val lastName: String = ""
-)
